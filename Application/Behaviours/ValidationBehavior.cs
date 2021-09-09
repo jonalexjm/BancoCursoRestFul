27cbac787a32d3eec17ad4f404589a911646730c
John Alexander Jimenez <john.jimenez@contratista.puntoscolombia.com>
2021-09-08 20:22:39 -0500

    manejo de exceptions via pipeline

---
+++ Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Behaviours
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validator;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> _validator)
+        {
+            _validator = this._validator;
+        }
+        public  async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validator.Any())
+            {
+                var context = new FluentValidation.ValidationContext<TRequest>(request);
+                var validationResults =
+                    await Task.WhenAll(_validator.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ Application/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FluentValidation.Results;
+
+namespace Application.Exceptions
+{
+    public class ValidationExceptions : Exception
+    {
+        public ValidationExceptions() : base("Se ha producido uno o mas errores de validacion")
+        {
+            Errors = new List<string>();
+        }
+
+        public List<string> Errors { get; }
+
+        public ValidationExceptions(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }    
+        } 
+        
+    }
+}
