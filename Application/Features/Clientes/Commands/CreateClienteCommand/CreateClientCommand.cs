6e5dff2896901be28bcccdadc0c615acd9f560b0
John Alexander Jimenez <john.jimenez@contratista.puntoscolombia.com>
2021-09-08 20:42:08 -0500

    Creando primera entidad de Dominio

---
+++ Application/Features/Clientes/Commands/CreateClienteCommand/CreateClientCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System;
+
+namespace Application.Features.Clientes.Commands.CreateClienteCommand
+{
+    public class CreateClientCommand : IRequest<Response<int>>
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }  
+    }
+}
---
+++ Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Common
+{
+    public abstract class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
