c4df58a7ab5c20252e1ca2d3c5bc0c4eb14f702a
John Alexander Jimenez <john.jimenez@contratista.puntoscolombia.com>
2021-09-08 19:58:56 -0500

    Configurando capa aplicacion

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection service)
+        {
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+        
+    }
+}
